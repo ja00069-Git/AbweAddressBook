10e5dbfa1d6a5f78dcb676059ebdb343d0b4905b
Jabesi Abwe <ja00069@my.westga.edu>
2024-02-07 11:54:17 -0500

    Implemented the contact class and DbContext class.

---
+++ AbweAddressBook/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbweAddressBook.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        [Required(ErrorMessage = "Please enter your fist name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your nickname")]
+        public string NickName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string PhoneNumber { get; set; }
+        public DateTime? DateCreated { get; set; } = DateTime.Now;
+    }
+}
---
+++ AbweAddressBook/Models/ContactContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AbweAddressBook.Models
+{
+    public class ContactContext: DbContext
+    {
+        public ContactContext(DbContextOptions<ContactContext> options) : base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; } = null!;
+    }
+}
