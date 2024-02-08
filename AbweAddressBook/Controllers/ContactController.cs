090ee8d5cf79864c35307c88d887cd5c07550ad6
Jabesi Abwe <ja00069@my.westga.edu>
2024-02-08 16:35:21 -0500

    Implement the controllers and home view (Index.cshtml)

---
+++ AbweAddressBook/Controllers/ContactController.cs
@@ -0,0 +1,78 @@
+﻿using AbweAddressBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AbweAddressBook.Controllers
+{
+    public class ContactController : Controller
+    {
+        private ContactContext Context { get; set; }
+
+        public ContactController(ContactContext ctx) => Context = ctx;
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            return View("Edit", new Contact());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            Contact? contact = Context.Contacts.Find(id);
+            return View(contact);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                if (contact.ContactId == 0)
+                {
+                    Context.Contacts.Add(contact);
+                }
+                else
+                {
+                    Context.Contacts.Update(contact);
+                }
+                Context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Action = (contact.ContactId == 0) ? "Add" : "Edit";
+                return View(contact);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Contact? contact = Context.Contacts.Find(id);
+            return View(contact);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Contact contact)
+        {
+            Context.Contacts.Remove(contact);
+            Context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            Contact? contact = Context.Contacts.Find(id);
+            return View(contact);
+        }
+
+        [HttpPost]
+        public IActionResult Details(Contact contact)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- AbweAddressBook/Controllers/HomeController.cs
+++ AbweAddressBook/Controllers/HomeController.cs
@@ -1,12 +1,18 @@
+using AbweAddressBook.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AbweAddressBook.Controllers
 {
     public class HomeController : Controller
     {
+        private ContactContext Context { get; set; }
+
+        public HomeController(ContactContext ctx) => Context = ctx;
+
         public IActionResult Index()
         {
-            return View();
+            IList<Contact> contacts = Context.Contacts.OrderBy(c => c.LastName).ThenBy(c => c.FirstName).ToList();
+            return View(contacts);
         }
       
     }
