6849d087abab2a18c804aca8d5b599bb4b5f1096
Jabesi Abwe <ja00069@my.westga.edu>
2024-02-09 12:50:17 -0500

    Modifed the Contact and Details page to make sure that the date created is just the date without time portion of it.

--- AbweAddressBook/Models/Contact.cs
+++ AbweAddressBook/Models/Contact.cs
@@ -7,16 +7,19 @@ namespace AbweAddressBook.Models
         public int ContactId { get; set; }
 
         [Required(ErrorMessage = "Please enter your fist name")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", ErrorMessage = "Numbers and special characters are not allowed in the first name.")]
         public string? FirstName { get; set; }
 
         [Required(ErrorMessage = "Please enter your last name")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", ErrorMessage = "Numbers and special characters are not allowed in the last name.")]
         public string? LastName { get; set; }
 
         public string? NickName { get; set; }
 
         [Required(ErrorMessage = "Please enter your phone number")]
+        [RegularExpression(@"^(\d{10})$", ErrorMessage = "Please enter a valid phone number")]
         public string? PhoneNumber { get; set; }
 
-        public DateTime? DateCreated { get; set; } = DateTime.Now.Date;
+        public DateTime? DateCreated { get; set; } = DateTime.Now;
     }
 }
